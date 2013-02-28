2272b4c7d20e460669587a325e18daf1c0d06abe
mitsugeek <mitsu322@gmail.com>
2013-03-01 07:24:33 +0900

    statusbar is hidden

--- fullScreenApp/FullScreenController.cs
+++ fullScreenApp/FullScreenController.cs
@@ -7,6 +7,9 @@ namespace fullScreenApp
 	{
 		public FullScreenController ()
 		{
+			//statusbar is hidden
+			UIApplication.SharedApplication.SetStatusBarHidden (true, UIStatusBarAnimation.None);
+
 			UIView view = new UIView (this.View.Frame);
 			view.BackgroundColor = UIColor.White;
 			this.View.AddSubview(view);
